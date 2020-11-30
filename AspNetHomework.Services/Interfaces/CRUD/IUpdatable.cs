faccb16a011a97b1a0f6927b1d70f6bade46dc35
D69ion <dmitriy.shtele@gmail.com>
2020-11-30 16:51:10 +0400

    Task 7 Реализация сервисов

---
+++ AspNetHomework.Services/Interfaces/CRUD/IUpdatable.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace AspNetHomework.Services.Interfaces.CRUD
+{
+    /// <summary>
+    /// Интерфейс сервиса для изменения сущности.
+    /// </summary>
+    /// <typeparam name="TDto">DTO.</typeparam>
+    public interface IUpdatable<TDto>
+    {
+        /// <summary>
+        /// Изменение сущности.
+        /// </summary>
+        /// <param name="dto">DTO.</param>
+        /// <returns>Обновленная сущность.</returns>
+        Task<TDto> UpdateAsync(TDto dto);
+    }
+}
--- AspNetHomework.Services/Interfaces/IProductService.cs
+++ AspNetHomework.Services/Interfaces/IProductService.cs
@@ -1,13 +1,12 @@
 ﻿using AspNetHomework.Models;
-using System.Collections.Generic;
+using AspNetHomework.Services.Interfaces.CRUD;
 
 namespace AspNetHomework.Services.Interfaces
 {
     /// <summary>
-    /// Интерфейс сервиса для работы с данными о товарах. 
+    /// Интерфейс сервиса для работы с данными о товарах.
     /// </summary>
-    public interface IProductService
+    public interface IProductService : ICrudService<ProductDTO>
     {
-        IEnumerable<ProductDTO> Get();
     }
 }
